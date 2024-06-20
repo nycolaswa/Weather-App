ca4660854b9f58be134205c986c9c7a957e443f8
Nycolase <nycolaswa@gmail.com>
2024-06-20 20:11:17 -0300

    interface

---
+++ Weather App/Color+Extensions.swift
@@ -0,0 +1,15 @@
+//
+//  Color+Extensions.swift
+//  Weather App
+//
+//  Created by Nycolas William de Andrade on 20/06/24.
+//
+
+import Foundation
+import UIKit
+
+extension UIColor {
+    static let contrastColor = UIColor(named: "contrastColor")
+    static let primaryColor = UIColor(named: "primaryColor")
+    static let softGray = UIColor(named: "softGray")
+}
